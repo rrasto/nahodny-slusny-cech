1219ee131fdf0efb1a7e0118901b6ea1ee277494
granci <granci@gmail.com>
2017-05-25 23:04:55 +0200

    Update generator.js

--- generator.js
+++ generator.js
@@ -190,8 +190,8 @@ function changeBackground() {
   var colors = ["(53,130,0)","(78,130,0)","(102,130,0)","(128,129,0)","(129,107,0)","(129,82,0)","(144,77,0)",
                 "(159,69,0)","(174,58,0)","(189,43,0)","(204,26,0)"];
   var images = ["http://img.topky.sk/big/1736737.jpg"];
-  //document.body.style.background = "rgb" + colors[rage];
-  document.getElementByClassName('container').style.background = "rgb" + colors[rage];
+  document.body.style.background = "rgb" + colors[rage];
+  //document.getElementByClassName('container').style.background = "rgb" + colors[rage];
 }
 
 function generatePost() {
