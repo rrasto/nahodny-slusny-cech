6a72ea8ccdbb07d0a4da85ea1e09d1e3cf2c32ec
granci <granci@gmail.com>
2017-05-25 22:27:57 +0200

    Update generator.js

--- generator.js
+++ generator.js
@@ -189,6 +189,7 @@ function changeBackground() {
   var rage = parseInt(document.getElementById('rageFactor').value);
   var colors = ["(53,130,0)","(78,130,0)","(102,130,0)","(128,129,0)","(129,107,0)","(129,82,0)","(144,77,0)",
                 "(159,69,0)","(174,58,0)","(189,43,0)","(204,26,0)"];
+  var images = ["http://img.topky.sk/big/1736737.jpg"];
   //document.body.style.background = "rgb" + colors[rage];
   document.getElementByClassName('container').style.background = "rgb" + colors[rage];
 }
