b4361c35ea97390ba6867534e5d3b5244a4f0fe7
granci <granci@gmail.com>
2017-05-27 13:10:30 +0200

    Update generator.js

--- generator.js
+++ generator.js
@@ -98,9 +98,9 @@ function getRandomSentence() {
     var item = badGuysSentences.splice(Math.floor(Math.random() * badGuysSentences.length), 1);
 //     console.log("item:");
 //     console.log(item);
-    console.log("sentenceTemplate");
+//     console.log("sentenceTemplate");
     var sentenceTemplate = item[0][1];
-    console.log(sentenceTemplate);
+//     console.log(sentenceTemplate);
 //     return item[0][1];
     return sentenceTemplate.formatUnicorn(shuffle(badGuys));
   } else {
